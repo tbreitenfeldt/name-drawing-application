fbdef36ea090fbfe661d69bf8277259ef973e62b
tbreitenfeldt <timothyjb310@gmail.com>
2022-11-20 14:30:45 -0800

    Updated the configuration json file, to include credentials, and refactored the email and deletion processes to use different libraries that function better with the new GMail security polacies.

--- README.md
+++ README.md
@@ -2,15 +2,20 @@
 
 An accessible name drawing program written in Python 3.8.6. This application is used for a secret Santa drawing that is done durring Christmas.
 
-This application takes a list of dictionaries that represent each individual involved in the name drawing, and will randomly assign each person a name from amungst those individuals who are participating. The application insures that an individual will not draw themselves, and that everyone gets drawn. The output of this application is an email to each person involved in the drawing, so a GMail account that has Less secure app access enabled is required. A JSON file is used to easily change the participants in the drawing. You must include a name and email for each object in the array of participants.
+This application takes a list of dictionaries that represent each individual involved in the name drawing, and will randomly assign each person a name from amungst those individuals who are participating. The application insures that an individual will not draw themselves, and that everyone gets drawn. The output of this application is an email to each person involved in the drawing, so a GMail account that has secure app access is required. A JSON file is used to easily change the participants in the drawing. You must include a name and email for each object in the array of participants.
 
 ## How to Run
 
-The most important part is the JSON file, which has all of the information about the individuals participating in the name drawing. Since this application uses emails as output, names, email, and an optional attachment field must be provided for each person. The file by default is expected to be participants.json, although you can pass a filename as a commandline argument into the application.  
+The most important part is the JSON file, which has all of the information about the individuals participating in the name drawing. Since this application uses emails as output, names, email, and an optional attachment field must be provided for each person. The file by default is expected to be config.json, although you can pass a filename as a commandline argument into the application.  
 example:
 
 ```
-[
+{
+"gmail_credentials": {
+"username": "email@gmail.com",
+"password": "app_password"
+},
+"participants": [
 {
 "name": "name1",
 "email": "name1@gmail.com"
@@ -28,11 +33,12 @@ example:
 "email": "name4@gmail.com"
 }
 ]
+}
 ```
 
 You can indent your JSON, as long as your JSON is valid.
 
-Note that this application is using GMail for sending the names, you need to enable less secure app access for the GMail account that is being used. The application does not save the username and password for your email account, so it will request you to enter your username and password every time.
+Note that this application is using GMail for sending the names, you need to generate an app password for this application for the GMail account that is being used. To generate an app password, you must first enable 2 factor authentication for your account, then you can generate a password to be used for this application.
 
 After sending the emails with the names to each participant, the program will connect to IMAP, and clear your sent mail/Trash to insure you do not know who got sent which name.
 
--- main.py
+++ main.py
@@ -4,47 +4,57 @@ version: Python 3.8.6
 description: Draws names based on a JSON file, and sends an email to the respective individual.
 """
 
-import random 
+import random
 import getpass
 import os
 import sys
 from typing import Dict, List
+import imaplib
+
+import yagmail
 
 import json_utils
-from email_manager import EmailManager
+
 
 def main(args: List[str]) -> None:
-    filename: str = "participants.json"
+    filename: str = "config.json"
 
     if len(args) == 2:
         filename = args[1]
 
     print("Name Drawing")
 
-    participants: List[Dict[str, str]] = get_participants(filename)
+    config: Dict[str, any] = read_json_config(filename)
+    participants: List[Dict[str, str]] = config["participants"]
     names: List[str] = extract_names(participants)
-    sending_email_credentials: Dict[str, str] = read_sending_email_credentials()
+    sending_email_credentials: Dict[str, str] = config["gmail_credentials"]
 
     print("Drawing names...")
     draw_names(participants, names)
     print("Sending emails with names...")
     send_emails_with_names(sending_email_credentials, participants)
+    permanently_remove_sent_mail(sending_email_credentials)
 
-def get_participants(filename) -> List:
+
+def read_json_config(filename) -> Dict:
     try:
         return json_utils.read_json(filename)
     except FileNotFoundError:
         print(f"Unable to find  the file \"{filename}\"")
         print("The file should be in the format:")
-        print("[\n{\n\"name\": \"example name\",\n\"email\": \"example@domain.com\"\n}\n]")
+        print(
+            "{\n\"gmail_credentials\": {\n\"username\": \"\",\,\"password\": \"\"\n},\n\"participants\": [\n{\n\"name\": \"example name\",\n\"email\": \"example@domain.com\"\n}\n]\n}")
         sys.exit()
+
 
 def extract_names(participants: List[Dict[str, str]]) -> List[str]:
     return [participant["name"] for participant in participants]
 
+
 def draw_names(participants: List[Dict[str, str]], names: List[str]) -> List[Dict[str, str]]:
     if len(participants) != len(names):
-        raise ValueError(f"the participants and names arrays must be equal, participants have {str(len(participants))} and names has {str(len(names))}")
+        raise ValueError(
+            f"the participants and names arrays must be equal, participants have {str(len(participants))} and names has {str(len(names))}")
     if len(participants) == 1:
         raise ValueError("Need at least two participants to draw.")
 
@@ -71,29 +81,33 @@ def draw_names(participants: List[Dict[str, str]], names: List[str]) -> List[Dic
 
     return participants
 
-def read_sending_email_credentials() -> Dict[str, str]:
-    username: str = input("Enter Sending email account username: ")
-    password: str = getpass.getpass("Enter Password: ")
-    return {"username": username, "password": password}
 
 def send_emails_with_names(credentials: Dict[str, str], participants: List[Dict[str, str]]) -> None:
-    email_manager: EmailManager = EmailManager()
-    smtp_server_information: Dict = {"server": "smtp.gmail.com", "port": 587}
-    imap_server_information: Dict[str, str] = {"server": "imap.gmail.com"}
-    email_manager.open_smtp_connection(smtp_server_information, credentials)
-    email_manager.open_imap_connection(imap_server_information, credentials)
+    with yagmail.SMTP(credentials["username"], credentials["password"]) as yag:
+        for participant in participants:
+            print(
+                f"Sending email to {participant['name']} at {participant['email']}...")
+            sending_address: str = credentials["username"]
+            to: str = participant["email"]
+            body: str = f"Hi {participant['name']},\nYou drew {participant['drawn_name']}\n\nThis is an automated message, do not respond."
+            subject: str = "Name Drawing"
+            yag.send(to, subject, body)
 
-    for participant in participants:
-        print(f"Sending email to {participant['name']} at {participant['email']}...")
-        sending_address: str = credentials["username"]
-        to: str = participant["email"]
-        body: str = f"Hi {participant['name']},\nYou drew {participant['drawn_name']}\n\nThis is an automated message, do not respond."
-        subject: str = "Name Drawing"
-        email_manager.send_email(sending_address, to, subject, body)
 
-    email_manager.delete_all_emails("Trash")
-    email_manager.close_smtp_connection()
-    email_manager.close_imap_connection()
+def permanently_remove_sent_mail(credentials: Dict[str, str]):
+    connection = imaplib.IMAP4_SSL('imap.gmail.com')
+    connection.login(credentials["username"], credentials["password"])
+    # delete sent mail
+    connection.select(mailbox='"[Gmail]/Sent Mail"')
+    connection.store("1:*", '+FLAGS', '\\Deleted')
+    connection.expunge()
+    # delete trash
+    connection.select(mailbox='"[Gmail]/Trash"', readonly=False)
+    connection.store("1:*", '+FLAGS', '\\Deleted')
+    connection.expunge()
+    connection.close()
+    connection.logout()
+
 
 if __name__ == "__main__":
     main(sys.argv)
